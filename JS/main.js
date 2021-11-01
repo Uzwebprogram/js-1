20ccd90a51b1bf44ec5adf3ccb9a5907f2fd1ab3
saidbek04 <atambayevsaid84@gmail.com>
2021-11-01 16:39:01 +0300

    js-1

---
+++ JS/main.js
@@ -0,0 +1,21 @@
+var name = prompt("😊ismingizni kiriting😊")
+var surname = prompt("😃Familyangizni kiriting😃")
+var year = prompt("🎉Tug'ilgan yilingizni kiriting🎉")
+var from = prompt ("✨Qaysi davlat fuqarosisiz✨")
+var live = prompt ("🏦Qaysi shaharda istiqomat qilasiz🏦")
+var study = prompt ("📖O'qiysizmi , ishlaysizmi ?📖") 
+var telefon = prompt ("📱Telefon raqamingizni kiriting📱")
+var life = prompt ("🤑Kelajakda kim bo'lmoqchsiz🤑")
+var car = prompt ("🚙 moshinagiz bormi ?🚗" )
+var family = prompt(" 👴 oilada nechta farzandsizlar ? 👵")  
+var dream = prompt ("🐱‍🏍orzularingizdan birni kiriting🐱‍🏍")
+var goal = prompt ("🎇Oldizga qo'ygan maqsadingiz 🎇")
+// alret start
+alert ("1." + name +   " " + surname +"😊" + "\n" + "2." + year +"😃"  + "\n"+"3." + 
+from + "🎉" + "\n" +"4." + live + "✨"+ "\n" + "5."+ study +"🏦"+ "\n"+"6." + telefon + "📱" + "\n" + "7." + car + "🚙" + "\n" + "8." + family + "👴"  + "\n" + "9." + dream + "🐱‍🏍" + "\n" + "10." + goal + "🎇" )
+// alret end
+
+// consol start
+console.log("1." + name +   " " + surname +"😊" + "\n" + "2." + year +"😃"  + "\n"+"3." + 
+from + "🎉" + "\n" +"4." + live + "✨"+ "\n" + "5."+ study +"🏦"+ "\n"+"6." + telefon + "📱" + "\n" + "7." + car + "🚙" + "\n" + "8." + family + "👴"  + "\n" + "9." + dream + "🐱‍🏍" + "\n" + "10." + goal + "🎇");
+// consol end
